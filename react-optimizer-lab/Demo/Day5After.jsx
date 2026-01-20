ec442ddc12dba6c24460b66f39104b2c80ab5750
Rudra Arora <arorarudra.it11@gmail.com>
2026-01-20 18:30:56 +0530

    last push

---
+++ react-optimizer-lab/Demo/Day5After.jsx
@@ -0,0 +1,18 @@
+import React, { Suspense, useState } from "react";
+
+//
+
+export default function Day5After() {
+  //
+
+  return (
+    <div className="card">
+      <h3>Day 5 (After): Lazy load only when needed</h3>
+      <button className="btn" onClick={() => setOpen(true)}>
+        Open Big Widget
+      </button>
+      <hr />
+      {/* */}
+    </div>
+  );
+}
---
+++ react-optimizer-lab/Demo/Day5Before.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+
+//
+
+//
+
+{
+  /* */
+}
+export default function Day5Before() {
+  return (
+    <div className="card">
+      <h3>Day 5 (Before): Big widget loads immediately</h3>
+      {/* */}
+      <hr />
+      {/* */}
+      <div className="small">
+        Problem: heavy code runs even if user doesn’t open it.
+      </div>
+    </div>
+  );
+}
